96a059dd26768a8e578ef912ca8e6bd3a8e9bc17
Edward <edward1791c@gmail.com>
2025-05-28 14:12:38 -0300

    test(login-account): add complete login test for valid credentials

--- cypress/e2e/specs/login.cy.js
+++ cypress/e2e/specs/login.cy.js
@@ -1,15 +1,32 @@
 import { loginPage } from "../pages/loginPage";
 
-
 describe('Login flow', () => {
     beforeEach(() => {
         loginPage.visit();
     });
 
     it('Allows the user to successfully log in', () => {
+        const email = Cypress.env('userEmail');
+        const password = Cypress.env('userPassword');
+        //fill the login form
         loginPage.getLoginTitle().should('be.visible');
-        loginPage.getEmailInput().type('fallaperoesfeature+test@gmail.com');
-        loginPage.getPasswordInput().type('Ax9p#2Ld');
+        loginPage.getEmailInput().type(email);
+        loginPage.getPasswordInput().type(password);
         loginPage.getLoginButton().click();
+        // Verifies that the user was successfully redirected and key dashboard elements are visible
+        cy.origin('https://opencart.abstracta.us', () => {
+            cy.url().should('include', '/account');
+            const headers = [
+                'My Account',
+                'My Orders',
+                'My Affiliate Account',
+                'Newsletter'
+            ];
+            headers.forEach(text => {
+                cy.get('h2').contains(text).should('be.visible');
+            });
+        });
+
+
     });
 });
